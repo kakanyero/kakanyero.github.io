eeb627a67f1ead629dd98035c21a5c5a975d775d
kakanyero <kakanyero142@gmail.com>
2023-09-07 15:33:00 +0300

    portfolio project

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript code can be added here if needed
+// For example, you can use JavaScript to handle button clicks or other interactions
+document.getElementById('visitApp').addEventListener('click', function () {
+    // Redirect to your deployed app URL when the "Visit App" button is clicked
+    window.location.href = 'https://yourdeployedapp.com';
+});
